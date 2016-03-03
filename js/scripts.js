b7575c532d199aca79cf6da6f1e9303a108d7457
Danny Pineda and Sarah Read Brown <dcorn13@gmail.com>
2016-03-02 20:33:53 -0800

    count down from entered number

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,27 @@
-// var bottleCount = function(n) {
-//
-// }
+var bottleCount = function(n) {
+  var text = "";
+  for (i = n ; i > 1; i--) {
+    text = text + i + "b"
+  }
+  return text;
+}
 
 
 $(document).ready(function() {
   $("form#bottles").submit(function(event) {
     var n = $("input#beers").val();
-    var m = (n - 1)
-    $(".n").text(n);
-    $(".nMinusOne").text(m);
+
+    var result = bottleCount(n)
+
+
+
+    $(".n").text(result);
+    // $(".nMinusOne").text(m);
 
     $("#result").show();
     event.preventDefault();
   });
 });
+
+// var song = "";
+// text += n + "bottles of beer on the wall" + n + "bottles of beer..." + "<br>" + "take one down, pass it around," + n[i] + "bottles of beer on the wall."
