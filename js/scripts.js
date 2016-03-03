a2457fb736aa6bd68d9168cda36314427a4fb614
Danny Pineda and Sarah Read Brown <sreadbrown@gmail.com>
2016-03-02 19:40:36 -0800

    sing the first line of the song based on the input

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,14 @@
-$(document).ready(function() {
-  $("form#piglatin").submit(function(event) {
-    var word = $("input#word").val();
-    var result = piglatin(word);
+// var bottleCount = function(n) {
+//
+// }
 
-    $("#translated").text(result);
+
+$(document).ready(function() {
+  $("form#bottles").submit(function(event) {
+    var n = $("input#beers").val();
+    var m = (n - 1)
+    $(".n").text(n);
+    $(".nMinusOne").text(m);
 
     $("#result").show();
     event.preventDefault();
